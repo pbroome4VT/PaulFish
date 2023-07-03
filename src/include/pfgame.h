84b8a840bd3b18f9d81bb7829eddf5bc14415f09
Paul Broome <pbroome4@me.com>
2023-07-03 13:59:31 -0400

    started work on bb

--- src/include/pfgame.h
+++ src/include/pfgame.h
@@ -1,7 +1,28 @@
 #ifndef PFGAME_H
 #define PFGAME_H
 
+#include <stdint.h>
 
+#define GAME_MAX_MOVES (371)
+
+typedef enum PlayerEnum player_t;
+enum PlayerEnum {Black = 0;
+
+typedef struct GameFrameStruct GameFrame;
+struct GameFrameStruct{
+	char captures [2];
+	uint64_t horzBB[7];
+	uint64_t vertBB[7];
+	uint64_t diag1BB[7]; /* /  */
+	uint64_t diag2BB[7] /*  \  */
+	uint64_t horzChebyshev1BB[7];		
+	uint64_t horzChebyshev2BB[7];
+};
+
+typedef struct GameStruct Game;
+struct GameStruct{
+	
+};
 
 
 
